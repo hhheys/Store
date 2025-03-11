a5a3c702d1b1dbd65f035e2d511a1e7334ba585c
Тимофей <tl79266837738@gmail.com>
2025-03-11 15:06:50 +0300

    Added product list by manufacturer

--- app/manufacturers/routes.py
+++ app/manufacturers/routes.py
@@ -7,9 +7,12 @@ from starlette.exceptions import HTTPException
 from starlette.requests import Request
 from starlette.responses import RedirectResponse
 
+from app.category.accessor import get_category_by_id
 from app.manufacturers.accessor import get_manufacturer_by_id, create_manufacturer
+from app.price.accessor import get_current_price
 from app.products.accessor import get_product_by_manufacturer
-from app.utils.cookies_session import get_current_admin
+from app.products.models import ProductCard
+from app.utils.cookies_session import get_current_admin, get_current_user
 from app.utils.utils import templates, save_manufacturer_image
 
 router = APIRouter(
@@ -20,10 +23,33 @@ class ManufacturerCreate(BaseModel):
     manufacturer_name: str
 
 @router.get("")
-async def manufacturer_by_id(man_id:int):
-    print(await get_manufacturer_by_id(man_id))
-    print(await get_product_by_manufacturer(man_id))
-    return {}
+async def manufacturer_by_id(request: Request, man_id:int, user = Depends(get_current_user)):
+    manufacturer = await get_manufacturer_by_id(man_id)
+    if not manufacturer:
+        raise HTTPException(status_code=404)
+    products = await get_product_by_manufacturer(man_id)
+    data = []
+    for product_el in products:
+        price = await get_current_price(product_el.id)
+        manufacturer = await get_manufacturer_by_id(product_el.manufacturer_id)
+        category = await get_category_by_id(product_el.category_id)
+        data.append(
+            ProductCard (
+                product_el,
+                price,
+                manufacturer,
+                category
+            )
+        )
+    print(data)
+    return templates.TemplateResponse("products_by_manufacturer.html",
+                                      {"request": request,
+                                       "man_id": man_id,
+                                       "cards": data,
+                                       "manufacturer": manufacturer,
+                                       "current_user": user
+                                       })
+
 
 @router.get("/create")
 async def create_manufacturer_template(request:Request, admin = Depends(get_current_admin)):
